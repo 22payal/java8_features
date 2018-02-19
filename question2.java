a9ca7c28489cce800b6769c8b0e9ab31287e6cab
22payal <payalnigam1996@gmail.com>
2018-02-19 05:33:27 +0000

    Update question2.java

--- question2.java
+++ question2.java
@@ -21,14 +21,14 @@ class question2
         Employee object = new Employee(10, "Payal Nigam");
         String filename = "file.ser";
 
-        // Serialization
+        
         try
         {
-            //Saving of object in a file
+           
             FileOutputStream file = new FileOutputStream(filename);
             ObjectOutputStream out = new ObjectOutputStream(file);
 
-            // Method for serialization of object
+            
             out.writeObject(object);
 
             out.close();
@@ -46,14 +46,14 @@ class question2
 
         Employee object1 = null;
 
-        // Deserialization
+       
         try
         {
-            // Reading the object from a file
+            
             FileInputStream file = new FileInputStream(filename);
             ObjectInputStream in = new ObjectInputStream(file);
 
-            // Method for deserialization of object
+           
             object1 = (Employee)in.readObject();
 
             in.close();
